f7653d7b14828eb772f3c0bd807557b8c2e4eba4
kgpasta <kaustubh.garimella@gmail.com>
2014-11-02 16:39:37 -0600

    Added rudimentary interface

--- app/watson_consulting.py
+++ app/watson_consulting.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')	# maps '/' to index()
 def index():
-	return "up and running"
+	return render_template('index.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
